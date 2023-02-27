7a4bba65ac75c6da6f3d52fd7a08603a17705dfe
jayden <1263346845@qq.com>
2023-02-27 15:08:25 +0800

    实现 选择排序 冒泡排序 插入排序功能 并且使用对数期进行测试

---
+++ algorithmcode/src/main/java/com/jayden/a1_sort/A1_SelectionSort.java
@@ -0,0 +1,112 @@
+package com.jayden.a1_sort;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ */
+public class A1_SelectionSort {
+
+    public static void selectSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr= new int[]{3,-1,-1,-4,1,5,9};
+//        selectSort(arr);
+//        for (int i : arr) {
+//            System.out.print(i + " ");
+//        }
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArr(arr1);
+            selectSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArr(arr1);
+                printArr(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArr(arr);
+        selectSort(arr);
+        printArr(arr);
+    }
+
+
+
+//  以下都是对数器
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    private static int[] copyArr(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    private static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printArr(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ algorithmcode/src/main/java/com/jayden/a1_sort/A3_InsertSort.java
@@ -0,0 +1,106 @@
+package com.jayden.a1_sort;
+
+import java.util.Arrays;
+
+/**
+ * 插入排序
+ */
+public class A3_InsertSort {
+
+    public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    // 交换arr的i和j位置上的值,注意i和j必须不能是同一个位置
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = new int[]{3, -1, -1, -4, 1, 5, 9};
+//        insertSort(arr);
+//        for (int i : arr) {
+//            System.out.print(i + " ");
+//        }
+        int testTime = 5000;
+        int maxSize = 10;
+        int maxValue = 10;
+        boolean succeed = true;
+        for (int i = 1; i < testTime; i++) {
+            int[] arr1 = generateRandomArr(maxSize, maxValue);
+            int[] arr2 = copyArr(arr1);
+            insertSort(arr1);
+            comparator(arr2);
+            if (!equal(arr1, arr2)) {
+                succeed = false;
+                printArr(arr1);
+                printArr(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        int[] arr = generateRandomArr(maxSize, maxValue);
+        printArr(arr);
+        insertSort(arr);
+        printArr(arr);
+
+    }
+
+    private static void printArr(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        ;
+    }
+
+    private static boolean equal(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    private static int[] copyArr(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] copyArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copyArr[i] = arr[i];
+        }
+        return copyArr;
+    }
+
+    private static int[] generateRandomArr(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((Math.random() + 1) * maxSize)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((Math.random() + 1) * maxValue) - (int) ((Math.random() + 1) * maxValue);
+        }
+        return arr;
+    }
+}

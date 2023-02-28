6658cdd7ec69685db9207daa5f2c71e0ce76b7b7
jayden <1263346845@qq.com>
2023-02-28 16:33:23 +0800

    实现用双链表建立栈队列,以及用数组实现队列算法

---
+++ algorithmcode/src/main/java/com/jayden/a4_linkedlist/A12_DoubleListToStackAndQueue.java
@@ -0,0 +1,184 @@
+package com.jayden.a4_linkedlist;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 双向链表实现栈和队列,以及双向队列
+ */
+public class A12_DoubleListToStackAndQueue {
+    public static class Node<T> {
+        public T value;
+        public Node<T> next;
+        public Node<T> last;
+
+        public Node(T data) {
+            value = data;
+        }
+    }
+
+    public static class DoubleEndsQueue<T> {
+        public Node<T> head;
+        public Node<T> tail;
+
+        public void addFromHead(T value) {
+            Node<T> cur = new Node<>(value);
+            if (head == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                cur.next = head;
+                head.last = cur;
+                head = cur;
+            }
+        }
+
+        public void addFromBottom(T value) {
+            Node<T> cur = new Node<>(value);
+            if (head == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                tail.next = cur;
+                cur.last = tail;
+                tail = cur;
+            }
+        }
+
+        public T popFromHead() {
+            if (head == null) {
+                return null;
+            }
+            Node<T> cur = head;
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+                cur.next = null;
+                head.last = null;
+            }
+            return cur.value;
+        }
+
+        public T popFromBottom() {
+            if (tail == null) {
+                return null;
+            }
+            Node<T> cur = tail;
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                tail = tail.last;
+                tail.next = null;
+                cur.last = null;
+            }
+            return cur.value;
+        }
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+    }
+
+    public static class MyStack<T>{
+        private DoubleEndsQueue<T> stack;
+
+        public MyStack(){
+            stack = new DoubleEndsQueue<T>();
+        }
+
+        public void push(T data){
+            stack.addFromHead(data);
+        }
+
+        public T pop() {
+            return stack.popFromHead();
+        }
+
+        public boolean isEmpty() {
+            return stack.isEmpty();
+        }
+
+    }
+
+    public static class MyQueue<T>{
+        private DoubleEndsQueue<T> queue;
+
+        public MyQueue(){
+            queue = new DoubleEndsQueue<T>();
+        }
+
+        public void push(T data){
+            queue.addFromHead(data);
+        }
+
+        public T poll(){
+            return queue.popFromBottom();
+        }
+
+        public boolean isEmpty() {
+            return queue.isEmpty();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        int oneTestDataNum = 100;
+        int value = 10000;
+        int testTimes = 100000;
+        for (int i = 0; i < testTimes; i++) {
+            MyStack<Integer> myStack = new MyStack<>();
+            MyQueue<Integer> myQueue = new MyQueue<>();
+            Stack<Integer> stack = new Stack<>();
+            Queue<Integer> queue = new LinkedList<>();
+            for (int j = 0; j < oneTestDataNum; j++) {
+                int nums = (int) (Math.random() * value);
+                if (stack.isEmpty()) {
+                    myStack.push(nums);
+                    stack.push(nums);
+                } else {
+                    if (Math.random() < 0.5) {
+                        myStack.push(nums);
+                        stack.push(nums);
+                    } else {
+                        if (!isEqual(myStack.pop(), stack.pop())) {
+                            System.out.println("oops!");
+                        }
+                    }
+                }
+                int numq = (int) (Math.random() * value);
+                if (queue.isEmpty()) {
+                    myQueue.push(numq);
+                    queue.offer(numq);
+                } else {
+                    if (Math.random() < 0.5) {
+                        myQueue.push(numq);
+                        queue.offer(numq);
+                    } else {
+                        if (!isEqual(myQueue.poll(), queue.poll())) {
+                            System.out.println("oops!");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("finish!");
+    }
+
+    public static boolean isEqual(Integer o1, Integer o2) {
+        if (o1 == null && o2 != null) {
+            return false;
+        }
+        if (o1 != null && o2 == null) {
+            return false;
+        }
+        if (o1 == null && o2 == null) {
+            return true;
+        }
+        return o1.equals(o2);
+    }
+}
---
+++ algorithmcode/src/main/java/com/jayden/a4_linkedlist/A13_ArrayToStackAndQueue.java
@@ -0,0 +1,51 @@
+package com.jayden.a4_linkedlist;
+
+/**
+ * 数组实现栈和队列
+ */
+public class A13_ArrayToStackAndQueue {
+
+    public static class MyQueue {
+        private int[] arr;
+        private int pushIndex; //begin
+        private int pollIndex; //begin
+        private int size;
+        private int limit;
+
+        public MyQueue(int limit) {
+            arr = new int[limit];
+            pushIndex = 0;
+            pollIndex = 0;
+            size = 0;
+            this.limit = limit;
+        }
+
+        public void push(int value) {
+            if (size == limit) {
+                throw new RuntimeException("队列满了，不能再加了");
+            }
+            size++;
+            arr[pushIndex] = value;
+            pushIndex = getNextIndex(pushIndex);
+        }
+
+        public int poll() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列为空,无法取出");
+            }
+            size--;
+            int ans = arr[pollIndex];
+            pollIndex = getNextIndex(pollIndex);
+            return ans;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        private int getNextIndex(int pushIndex) {
+            return pushIndex < limit - 1 ? pushIndex++ : 0;
+        }
+    }
+
+}

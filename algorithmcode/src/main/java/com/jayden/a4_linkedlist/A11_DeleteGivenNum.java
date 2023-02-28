53aefd06c0f5a14affcb777ce43b9d319e44ce8a
jayden <1263346845@qq.com>
2023-02-28 11:45:39 +0800

    实现单双链表倒转, 以及将链表中给定值删除算法的实现

---
+++ algorithmcode/src/main/java/com/jayden/a4_linkedlist/A11_DeleteGivenNum.java
@@ -0,0 +1,32 @@
+package com.jayden.a4_linkedlist;
+
+public class A11_DeleteGivenNum {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node deleteGivenNum(Node head, int value) {
+        while (head != null) {
+            if (head.value!=value){
+                break;
+            }
+            head = head.next;
+        }
+        Node pre = head;
+        Node cur = head;
+        while (cur !=null){
+            if (cur.value==value){
+                pre.next = cur.next;
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}

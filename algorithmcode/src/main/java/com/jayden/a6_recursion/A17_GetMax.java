ae865b96dbb01d994d116aea536c690bba352a83
jayden <1263346845@qq.com>
2023-03-06 10:00:57 +0800

    实现归并排序,利用归并排序实现小和问题和逆序对问题

--- algorithmcode/src/main/java/com/jayden/a6_recursion/A17_GetMax.java
+++ algorithmcode/src/main/java/com/jayden/a6_recursion/A17_GetMax.java
@@ -7,18 +7,22 @@ public class A17_GetMax {
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 1, 4, 1, 5, 9,7,12,1};
-        int maxNumber = getMax(arr, 0, arr.length - 1);
+        int maxNumber = getMax(arr);
         System.out.println(maxNumber);
     }
 
+    public static int getMax(int[] arr){
+        return process(arr,0,arr.length-1);
+    }
+
     //递归
-    private static int getMax(int[] arr, int L, int R) {
+    private static int process(int[] arr, int L, int R) {
         if (L == R) {
             return arr[L];
         }
         int mid = L + ((R - L) >> 1);
-        int leftMax = getMax(arr, L, mid);
-        int rightMax = getMax(arr, mid + 1, R);
+        int leftMax = process(arr, L, mid);
+        int rightMax = process(arr, mid + 1, R);
         return Math.max(leftMax, rightMax);
     }
 }

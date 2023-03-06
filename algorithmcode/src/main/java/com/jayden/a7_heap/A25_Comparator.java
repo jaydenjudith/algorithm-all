a1ba8b105b5dd23df8a9b842557018657b633e35
jayden <1263346845@qq.com>
2023-03-06 18:45:04 +0800

    实现比较器, 大根堆实现和堆排序算法

--- algorithmcode/src/main/java/com/jayden/a7_heap/A25_Comparator.java
+++ algorithmcode/src/main/java/com/jayden/a7_heap/A25_Comparator.java
@@ -5,6 +5,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeMap;
 
+/**
+ * 实现比较器:
+ *
+ * 1  比较器的实质就是重载比较运算符
+ * 2 比较器可以很好的应用在特殊标准的排序上
+ * 3 比较器可以很好的应用在根据特殊标准排序的结构上
+ * 4 写代码变得异常容易，还用于范型编程
+ *
+ * 即如下方法：
+ * @Override
+ * public int compare(T o1, T o2) ;
+ * 返回负数的情况，就是o1比o2优先的情况
+ * 返回正数的情况，就是o2比o1优先的情况
+ * 返回0的情况，就是o1与o2同样优先的情况
+ */
 public class A25_Comparator {
 
     public static class IdShengAgeJiangOrder implements Comparator<Student> {
